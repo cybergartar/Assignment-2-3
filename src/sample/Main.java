df3c28b59e4be2e057fc28a40d57fe6247c54075
Cybergartar <kamonnop.a@gmail.com>
2016-10-17 13:27:50 +0700

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,23 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Button btOk = new Button("OK");
+
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(btOk, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
